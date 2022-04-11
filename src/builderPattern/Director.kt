b813d7480e4e1163f02c8bfaf0ac0c2f189a22fb
habibellah <habibayata1@gmail.com>
2022-04-11 06:45:13 +0100

    new pattern : Builder Pattern

---
+++ src/builderPattern/Director.kt
@@ -0,0 +1,19 @@
+package builderPattern
+/*
+we can call the Director class by the controller it will control the construct or building steps
+and give the final result
+ */
+class Director
+{
+
+    private lateinit var builderVehicle:IBuilder
+     fun constructVehicle(builderVehicle1 : IBuilder)
+    {
+        this.builderVehicle = builderVehicle1
+        builderVehicle1.startBuildVehicle()
+        builderVehicle1.buildBody()
+        builderVehicle1.insertWheels()
+        builderVehicle1.addHeadLights()
+        builderVehicle1.endBuildVehicle()
+    }
+}
---
+++ src/builderPattern/UserBuild.kt
@@ -0,0 +1,12 @@
+package builderPattern
+/*
+this example will know from it the builder pattern it will build a car
+and this car has pieces ,and we implement the concept of the pattern by this example
+ */
+fun main() {
+    val director =  Director()
+    val build =  Car("Toyota")
+    director.constructVehicle(build)
+    val vehicle = build.getVehicle()
+    vehicle.show()
+}
