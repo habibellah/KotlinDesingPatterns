4f35d497c78c766fb48c035c26b9ecdd22bde52b
habibellah <habibayata1@gmail.com>
2022-04-19 15:50:02 +0100

    new pattern : prototype Pattern

---
+++ src/protoType/IShape.kt
@@ -0,0 +1,8 @@
+package protoType
+/*here the clone method its work will return a new obj of one of the concrete classes
+who will implement it and with the same properties of the first one
+ */
+interface IShape {
+
+   fun  clone():IShape
+}
---
+++ src/protoType/User.kt
@@ -0,0 +1,18 @@
+package protoType
+/*
+this example of our prototype pattern is about shapes we have for example one rectangle
+,and we want to create new one with the same properties
+we use prototype pattern with clone method will create new one without create it from zero and maybe
+take time in other programmes for example take data from DB
+ */
+fun main() {
+
+val r1 = Rectangle()
+r1.high = 5
+    r1.width = 12
+    val r2 = r1.clone() as Rectangle
+    println("the high and the width of r2 is: ${r1.high} ${r1.width}")
+    println("the high and the width of r2 is: ${r2.high} ${r2.width}")
+
+}
+
